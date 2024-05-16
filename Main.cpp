13c24462079c3f3839668d1c3cdcc47ddeef8ddf
Kalil Teotonio <80278976+Galill@users.noreply.github.com>
2024-05-16 00:03:32 -0300

    Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,14 @@
 #include "FileReader.h"
+#include "Automato.h"
 
 using namespace std;
 
 int main() {
 	FileReader file;
+	Automato afd;
 
 	file.readFile();
+	afd.minimizarAutomato();
 
 	return 0;
 }
