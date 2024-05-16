e54215b39c4b26e89aa4561af150063475d3b707
Kalil Teotonio <80278976+Galill@users.noreply.github.com>
2024-05-16 00:01:05 -0300

    Automato.h

--- Automato.h
+++ Automato.h
@@ -1,1 +1,17 @@
 #pragma once
+#include <iostream>
+#include <string>
+#include <set>
+#include <map>
+
+class Automato {
+
+public:
+   static std::set<std::string> estados;
+   static std::set<char> alfabeto;
+   static std::set<std::string> estados_finais;
+   static std::set<std::string> estado_inicial;
+   static std::map<std::pair<std::string, std::pair<std::string, char>>, bool> transicoes;
+
+    void minimizarAutomato();
+};
